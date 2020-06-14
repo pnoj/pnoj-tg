07bd89b5b9ff51d9d6e15e69111ae6c464be5eb4
Paul Lee <enigma@enigmatic.network>
2020-06-14 14:14:43 -0400

    package for pypi

--- src/main.py
+++ src/main.py
@@ -20,18 +20,18 @@ def generate_batch(config, inputter_command, outputter_command):
     points = config['points']
     testcase_num_digits = int(math.log10(num_testcases))+1
 
-    batch_dir = "testcases/{0}".format(name)
+    batch_dir = os.path.join("testcases", name)
     os.mkdir(batch_dir)
     manifest = {'type': 'batch', 'name': name, 'metadata': {'points': points}}
-    with open("{0}/manifest.yaml".format(batch_dir), "w") as manifest_file:
+    with open(os.path.join(batch_dir, "manifest.yaml"), "w") as manifest_file:
         yaml.dump(manifest, manifest_file)
     for testcase_num in range(num_testcases):
         input_data, output_data = generate_testcase(inputter_command, outputter_command)
         testcase_name = testcases_name.format(str(testcase_num+1).zfill(testcase_num_digits))
-        os.mkdir("{0}/{1}".format(batch_dir, testcase_name))
-        with open("{0}/{1}/{1}.in".format(batch_dir, testcase_name), "w") as testcase_input_file:
+        os.mkdir(os.path.join(batch_dir, testcase_name))
+        with open(os.path.join(batch_dir, testcase_name, testcase_name + ".in"), "w") as testcase_input_file:
             testcase_input_file.write(input_data)
-        with open("{0}/{1}/{1}.out".format(batch_dir, testcase_name), "w") as testcase_output_file:
+        with open(os.path.join(batch_dir, testcase_name, testcase_name + ".out"), "w") as testcase_output_file:
             testcase_output_file.write(output_data)
 
 def main(config):
@@ -42,10 +42,13 @@ def main(config):
     for batch in batches:
         generate_batch(batch, inputter_command, outputter_command)
 
-if __name__ == "__main__":
+def cli():
     parser = argparse.ArgumentParser(description='Generate testdata in PNOJ format.')
     parser.add_argument('configuration', help='File name of the configuration YAML.')
     args = parser.parse_args()
     with open(args.configuration, "r") as config_file:
         config = yaml.safe_load(config_file)
     main(config)
+
+if __name__ == "__main__":
+    cli()
